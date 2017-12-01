b96913e65b770a4d4a68fabbfb3bf5f8529cb82e
Patrick Willmore <patman1127@gmail.com>
2017-12-01 14:10:32 -0700

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,79 @@
-
 // generate random number between 19-120
 var winCount;
 var lossCount;
-var crystalsArray = [0,0,0,0];
-var score = [];
+var crystalsArray = [0, 0, 0, 0];
+var score;
+var target;
 
-function randomNumber()
-{
-    return Math.floor((Math.random() * 120) + 1);
+function randomNumber() {
+  return Math.floor(Math.random() * 120 + 1);
 }
 
-$('.random').html(randomNumber);
+
 
 //each crystal needs random number between 1-12
-function randomCrystal()
-{
-    return Math.floor((Math.random() * 12) + 1);
-
+function randomCrystal() {
+  return Math.floor(Math.random() * 12 + 1);
 }
 
 function initializeGame() {
-    winCount = 0;
-    lossCount = 0;
-    score = 0;
-    for (i = 0; i < crystalsArray.length; i++){
-            crystalsArray[i] = randomCrystal();
-         //   $('arrayOfimages'[i]).text(crystalsArray[i]);
-    }
-    console.log(crystalsArray);
-    // $('#ruby').(crystalsArray[0]);
-    // $('#diamond').text(crystalsArray[1]);
-    // $('#emerald').text(crystalsArray[2]);
-    // $('#topaz').text(crystalsArray[3]);
-    // var crystalValue = ($(this).attr("data-crystalvalue"));
-    // crystalValue = parseInt(crystalValue);
+  winCount = 0;
+  lossCount = 0;
+  score = 0;
+  target = randomNumber();
+  for (i = 0; i < crystalsArray.length; i++) {
+    crystalsArray[i] = randomCrystal();
+    //   $('arrayOfimages'[i]).text(crystalsArray[i]);
+    $(".random").html(target);
+    $("#RunningScore").text(score);
   }
+  console.log(crystalsArray);
+  // $('#ruby').(crystalsArray[0]);
+  // $('#diamond').text(crystalsArray[1]);
+  // $('#emerald').text(crystalsArray[2]);
+  // $('#topaz').text(crystalsArray[3]);
+  // var crystalValue = ($(this).attr("data-crystalvalue"));
+  // crystalValue = parseInt(crystalValue);
+}
 
 initializeGame();
 
 //onclick button should add each value to total score
 
-$("#ruby").on("click",function(){
-    score = score + crystalsArray[0];
-    $('#RunningScore').text(score);
+$("#ruby").on("click", function() {
+  score = score + crystalsArray[0];
+  $("#RunningScore").text(score);
+  checkScore();  
 });
-$("#diamond").on("click",function(){
-    score = score + crystalsArray[1];
-    $('#RunningScore').text(score);
+$("#diamond").on("click", function() {
+  score = score + crystalsArray[1];
+  $("#RunningScore").text(score);
+  checkScore();  
 });
-$("#emerald").on("click",function(){
-    score = score + crystalsArray[2];
-    $('#RunningScore').text(score);
+$("#emerald").on("click", function() {
+  score = score + crystalsArray[2];
+  $("#RunningScore").text(score);
+  checkScore();  
 });
-$("#topaz").on("click",function(){
-    score = score + crystalsArray[3];
-    $('#RunningScore').text(score);
+$("#topaz").on("click", function() {
+  score = score + crystalsArray[3];
+  $("#RunningScore").text(score);
+  checkScore();  
 });
-
-
+console.log(score);
+function checkScore() {
+  if (score === target) {
+    winCount++;
+    alert("You Win!");
+    $("#winNumber").text(winCount);
+    initializeGame();
+  } else if (score > target) {
+    alert("You Lose!");
+    lossCount++;
+    $("#lossNumber").text(lossCount);
+    initializeGame();
+  }
+}
 //if totalScore === random number wins++
 
 //if totalscore > random number losses++
